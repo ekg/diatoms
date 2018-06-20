bd49d655b712934ddae66013268f6d58b8864fc3
Erik Garrison <erik.garrison@gmail.com>
2018-06-20 11:42:33 +0100

    filter output with <2 alleles

--- plot-ld.sh
+++ plot-ld.sh
@@ -5,7 +5,7 @@
 x=$1
 y=$2
 z=$3
-tomahawk view -i pmulti.monotig.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:$x-$y -a 0 | grep -v "^#" | awk 'BEGIN { OFS ="\t" } ($3 >= '$x' && $5 >= '$x' && $3 < '$y' && $5 < '$y') { print } NR==1 { print }' >monotig_$x-$y.$z.ld
+tomahawk view -i pmulti.monotig.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:$x-$y -a 2 | grep -v "^#" | awk 'BEGIN { OFS ="\t" } ($3 >= '$x' && $5 >= '$x' && $3 < '$y' && $5 < '$y') { print } NR==1 { print }' >monotig_$x-$y.$z.ld
 ./ld-plot-any.square.special.R monotig_$x-$y.$z.ld monotig_$x-$y.$z.ld.pdf
 convert -density 300 monotig_$x-$y.$z.ld.pdf monotig_$x-$y.$z.ld.png
 
