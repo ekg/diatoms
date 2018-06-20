896172068f0c8ef022aa9ca96de9d1121702ec07
Erik Garrison <erik.garrison@gmail.com>
2018-06-20 11:03:24 +0100

    things are working consistently

---
+++ plot-ld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#x=$(cat monotig.mapping.txt | grep -B 10 -A 11 tig00000855 | head -1 | cut -f 2)
+#y=$(cat monotig.mapping.txt | grep -B 10 -A 11 tig00000855  | tail -1 | cut -f 2)
+x=$1
+y=$2
+z=$3
+tomahawk view -i pmulti.monotig.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:$x-$y -a 0 | grep -v "^#" | awk 'BEGIN { OFS ="\t" } ($3 >= '$x' && $5 >= '$x' && $3 < '$y' && $5 < '$y') { print } NR==1 { print }' >monotig_$x-$y.$z.ld
+./ld-plot-any.square.special.R monotig_$x-$y.$z.ld monotig_$x-$y.$z.ld.pdf
+convert -density 300 monotig_$x-$y.$z.ld.pdf monotig_$x-$y.$z.ld.png
+
+
--- tomahawk-monotig.sh
+++ tomahawk-monotig.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 
-cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel -k -j 28 'tabix -h pmulti.vcf.gz {} | bcftools filter -i "QUAL > 1" | vcfallelicprimitives | python3 order-vcf.py monotig.mapping.txt monotig ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta' | vcffirstheader | vcf-sort -c | bgziptabix pmulti.monotig.Q1.norm.vcf.gz
+time cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel -k -j 28 'tabix -h pmulti.vcf.gz {} | bcftools filter -i "QUAL > 1" | vcfallelicprimitives | python3 order-vcf.py monotig.mapping.txt monotig ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta' | vcffirstheader | vcf-sort -c | bgziptabix pmulti.monotig.Q1.norm.vcf.gz
 time bcftools view -Ob pmulti.monotig.Q1.norm.vcf.gz >pmulti.monotig.Q1.norm.bcf
-time tomahawk import -i pmulti.Q1.norm.bcf -o pmulti.Q1.norm -n 1.0 
+time tomahawk import -i pmulti.monotig.Q1.norm.bcf -o pmulti.monotig.Q1.norm -n 1.0 
 time tomahawk calc -pdi pmulti.monotig.Q1.norm.twk -o pmulti.monotig.Q1.norm.calc -a 0 -r 0.1 -P 0.1 -t 28
 time tomahawk sort -i pmulti.monotig.Q1.norm.calc.two -o pmulti.monotig.Q1.norm.calc_psort
 time tomahawk sort -i pmulti.monotig.Q1.norm.calc_psort.two -o pmulti.monotig.Q1.norm.calc_sorted -M
-time tomahawk view -i pmulti.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:34512712-39515908 -a 0 | grep -v "^#" >monotig:34512712-39515908.ld
-time ./ld-plot-any.R monotig:34512712-39515908.ld monotig:34512712-39515908.ld.pdf
-
-
+#time tomahawk view -i pmulti.monotig.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:34512712-39515908 -a 0 | grep -v "^#" >monotig:34512712-39515908.ld
+#time ./ld-plot-any.square.R monotig:34512712-39515908.ld monotig:34512712-39515908.ld.pdf
