070d6f57870c70c4f38c681083d48717dfb6add9
Erik Garrison <erik.garrison@gmail.com>
2018-06-20 19:40:59 +0100

    set the datashader rendering dimension

--- shader-ld.py
+++ shader-ld.py
@@ -8,8 +8,9 @@ import sys
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
+dimension = int(sys.argv[3])
 df = pd.read_csv(infile, sep="\t", compression='gzip')
-cvs = ds.Canvas(plot_width=1000, plot_height=1000)
+cvs = ds.Canvas(plot_width=dimension, plot_height=dimension)
 agg = cvs.points(df, 'POS_A', 'POS_B', ds.mean('R2'))
 img = ds.transfer_functions.set_background(tf.shade(agg, cmap=['white', 'blue', 'red']), color="white")
 export_image(img, outfile)
