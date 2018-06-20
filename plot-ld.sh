cf3772a8c0af619b25b6ee44519f1019c10dda62
Erik Garrison <erik.garrison@gmail.com>
2018-06-20 14:43:21 +0100

    trying to fix import problem

--- plot-ld.sh
+++ plot-ld.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#x=$(cat monotig.mapping.txt | grep -B 10 -A 11 tig00000855 | head -1 | cut -f 2)
-#y=$(cat monotig.mapping.txt | grep -B 10 -A 11 tig00000855  | tail -1 | cut -f 2)
-x=$1
-y=$2
-z=$3
+x=$1 # lower limit
+y=$2 # upper limit
+z=$3 # version of plot
+monotigs=$4
 tomahawk view -i pmulti.monotig.Q1.norm.tomahawk.calc_sorted.two -N -I monotig:$x-$y -a 2 | grep -v "^#" | awk 'BEGIN { OFS ="\t" } ($3 >= '$x' && $5 >= '$x' && $3 < '$y' && $5 < '$y') { print } NR==1 { print }' >monotig_$x-$y.$z.ld
-./ld-plot-any.square.special.R monotig_$x-$y.$z.ld monotig_$x-$y.$z.ld.pdf
+./ld-plot-any.square.special.R monotig_$x-$y.$z.ld monotig_$x-$y.$z.ld.pdf $monotigs
 convert -density 300 monotig_$x-$y.$z.ld.pdf monotig_$x-$y.$z.ld.png
 
 
--- tomahawk-base.sh
+++ tomahawk-base.sh
@@ -2,16 +2,25 @@
 
 
 
-cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel -k -j 28 'tabix -h pmulti.vcf.gz {} | vcfremovesamples - pool40.MT- pool40.MT+ | bcftools filter -i "QUAL > 1" | vcfallelicprimitives' | vcffirstheader | vcf-sort -c | bgziptabix pmulti.Q1.norm.vcf.gz
+cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel -k -j 28 'tabix -h pmulti.vcf.gz {} | vcfremovesamples - pool40.MT- pool40.MT+ | vcffixup - | vcfkeepinfo - AF AC | vcfkeepgeno - GT | ./vcfnulldotslashdot-clean | bcftools filter -i "QUAL > 1" | vcfallelicprimitives | ./vcfnulldotslashdot-clean' | vcffirstheader | vcf-sort -c | bgziptabix pmulti.Q1.norm.vcf.gz
 time bcftools view -Ob pmulti.Q1.norm.vcf.gz >pmulti.Q1.norm.bcf
 time tomahawk import -i pmulti.Q1.norm.bcf -o pmulti.Q1.norm -n 1.0 -far
 time tomahawk calc -pdi pmulti.Q1.norm.twk -o pmulti.Q1.norm.calc -a 0 -t 28
 time tomahawk sort -i pmulti.Q1.norm.calc.two -o pmulti.Q1.norm.calc_psort
 time tomahawk sort -i pmulti.Q1.norm.calc_psort.two -o pmulti.Q1.norm.calc_sorted -M
-mkdir -p linkage_graph_mean && cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel './render-tig-linkage-mean.sh {} linkage_graph_mean/ pmulti.Q1.norm.calc_sorted.two && echo {}'
+rm -rf linkage_graph_mean && mkdir -p linkage_graph_mean && cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | parallel './render-tig-linkage-mean.sh {} linkage_graph_mean/ pmulti.Q1.norm.calc_sorted.two && echo {}'
 time find linkage_graph_mean -type f | parallel 'zcat {}' | sort --parallel=8 >pmulti.Q1.norm.linkage_graph_mean.tsv
 neighborly-tour/neighborly-tour pmulti.Q1.norm.linkage_graph_mean.tsv >pmulti.Q1.norm.linkage_graph_mean.tours
 head -1 pmulti.Q1.norm.linkage_graph_mean.tours | cut -f 2 | tr ',' '\n' | head -n-1 >uPseMul2.contig_order.txt
-( cat uPseMul2.contig_order.txt ; comm -13 <(sort uPseMul2.contig_order.txt ) <(cat ../diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | sort)  ) >uPseMul2.contig_order.plus_isolated.txt
-join <(cat uPseMul2.contig_order.plus_isolated.txt | awk '{ print $1, NR"."$0 }' | tr ' ' '\t' | sort ) <(cat ../diatom/assemblies/uPseMul2.contigs.fasta.fai ) | tr ' ' '\t' | cut -f 2,3  | sort -n | awk 'BEGIN { n=0; } { print $0, n; n+=$2 }' | tr ' ' '\t'  | cut -f 2 -d. | cut -f 1,3 >monotig.mapping.txt
+( cat uPseMul2.contig_order.txt ; comm -13 <(sort uPseMul2.contig_order.txt ) <(cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai | cut -f 1 | sort)  ) >uPseMul2.contig_order.plus_isolated.txt
+join <(cat uPseMul2.contig_order.plus_isolated.txt | awk '{ print $1, NR"."$0 }' | tr ' ' '\t' | sort ) <(cat ~/graphs/diatom/assemblies/uPseMul2.contigs.fasta.fai ) | tr ' ' '\t' | cut -f 2,3  | sort -n | awk 'BEGIN { n=0; } { print $0, n; n+=$2 }' | tr ' ' '\t'  | cut -f 2 -d. | cut -f 1,3 >monotig.mapping.txt
+
+
+
+
+
+
+
+
+
 
