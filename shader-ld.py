454971321797b80eca8c6efcfc8d5e5503769f49
Erik Garrison <erik.garrison@gmail.com>
2018-06-27 13:13:00 +0100

    plot functions

--- shader-ld.py
+++ shader-ld.py
@@ -2,14 +2,21 @@
 
 import datashader as ds
 import datashader.transfer_functions as tf
-from datashader.utils import export_image
-import pandas as pd
+from datashader.utils import export_imageq
+#import pandas as pd
+import dask_igzip
 import sys
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 dimension = int(sys.argv[3])
-df = pd.read_csv(infile, sep="\t", compression='gzip')
+#scaling = int(sys.argv[3]) # number of positions per pixel
+df = pd.read_csv(infile, sep="\t")
+#mins = df.min()
+#maxs = df.max()
+#width = abs(maxs['POS_A'] - mins['POS_A'])
+#height = abs(maxs['POS_B'] - mins['POS_B'])
+#dimension = int(round(max(height, width) / float(scaling)))
 cvs = ds.Canvas(plot_width=dimension, plot_height=dimension)
 agg = cvs.points(df, 'POS_A', 'POS_B', ds.mean('R2'))
 img = ds.transfer_functions.set_background(tf.shade(agg, cmap=['white', 'blue', 'red']), color="white")
---
+++ tomahawk_aggregate_count_raster.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+color_range<-11
+colors<-colorRampPalette(c("blue","red"))(color_range)
+#colors[2:5]<-paste0(colors[2:5],c(20,40,60,80))
+colors[1:2]<-"#FFFFFF"
+mat<-read.delim(commandArgs(TRUE)[1],h=F)
+# Linear transformation of data into percentile space for mapping the given
+# colorkey to this set
+#
+# Transform matrix values into 1-percentile bins
+dist<-table(cut(mat[mat>0],breaks=seq(0,max(mat[mat>0]),length.out = 101),include.lowest = T))
+plot(cumsum(dist/sum(dist)),type="o",pch=20)
+col_breaks<-rep(0,color_range-1)
+for(i in 1:(color_range-1)){
+  abline(v=which.max(cumsum(dist/sum(dist))>1 - 1/i))
+  # Compute 10-percentile bins using the nearest-rank method
+  col_breaks[i] = which.max(cumsum(dist/sum(dist))>1 - 1/i) / 100
+}
+
+imgsize <- as.numeric(commandArgs(TRUE)[3])
+#png(commandArgs(TRUE)[2], height=imgsize, width=imgsize)
+#image(as.matrix(mat),breaks = c(0, col_breaks, 1), col = colors,useRaster = T)
+#dev.off()
+
+# For associative count matrix
+mat<-read.delim(commandArgs(TRUE)[1],h=F)
+truncate <- as.numeric(commandArgs(TRUE)[5])
+mat2<-mat/truncate # Truncate at user-defined level
+mat2[mat2>1]<-1 # Everything over 1 squash to 1
+png(commandArgs(TRUE)[2], height=imgsize, width=imgsize)
+image(as.matrix(mat2),useRaster = T,axes=F)
+total_length <- as.numeric(commandArgs(TRUE)[4])
+axis(1, at = seq(0, total_length, by = 10e6)/total_length, labels = seq(0, total_length, by = 10e6)/1e6, las=2)
+axis(2, at = seq(0, total_length, by = 10e6)/total_length, labels = seq(0, total_length, by = 10e6)/1e6, las=2)
+dev.off()
